efabe5c494914e4303d276b77fccf0f61e01c2ac
Pan W <panthongw@gmail.com>
2015-10-08 19:17:25 -0400

    added posts and posts/new routes that write and return data

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+var postDB = [];
+
 app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-	var data = "{message: 'Hello World'}";
-	res.send(JSON.parse(data));
+app.get('/posts', function(req, res) {
+	res.setHeader('Cache-Control', 'no-cache');
+	res.send(postDB);
 });
 
-app.post('/new', function(req, res) {
-
+app.post('/posts/new', function(req, res) {
+	postDB.push(req.body);
+	res.send({status: 'success'});
 });
 
 app.listen(app.get('port'), function(){
